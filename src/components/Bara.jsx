acfdbe361f7116f3e1dabf242b8873837006482a
DreamSites-RO <dreamsites2024@gmail.com>
2024-12-01 12:46:31 +0200

    Commit

--- src/components/Bara.jsx
+++ src/components/Bara.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
 const Bara = () => {
   return (
-    <div className="h-[2px] bg-white/75 opacity-80 lg:w-[75%] w-[95%] mx-auto my-16"></div>
+    <motion.div
+      className="h-[2px] bg-white/75 opacity-80 lg:w-[75%] w-[95%] mx-auto my-16"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      whileExit={{ opacity: 0 }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{ duration: 1 }}
+    ></motion.div>
   );
 };
 
