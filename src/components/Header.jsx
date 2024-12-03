197ce473160434a624f827ce87582129ba63636f
DreamSites-RO <dreamsites2024@gmail.com>
2024-12-03 18:12:02 +0200

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,30 @@
 import { NavBar } from "../components";
-
 import React from "react";
-
 import VideoReady from "../assets/VideoReady.mp4";
+import { motion } from "framer-motion"; // Import motion from Framer Motion
 
 const Header = () => {
+  // Staggered animation variants
+  const staggerContainer = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.3, // Stagger time (delay between each item)
+      },
+    },
+  };
+
+  const fadeUp = {
+    hidden: { opacity: 0, y: 50 },
+    show: { opacity: 1, y: 0 },
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 25,
+      delay: 1, // 1 second delay before the animation starts
+    },
+  };
+
   return (
     <header className="relative w-full h-screen overflow-hidden">
       <video
@@ -20,12 +40,29 @@ const Header = () => {
       <div className="relative z-10 w-full">
         <NavBar />
         <div className="flex flex-col items-center justify-center h-full mt-20 text-center">
-          <h1 className="lg:text-[5.5rem] md:text=[4.3] text-[3.7rem] font-black mt-8 text-white">
-            Welcome to StarLight!
-          </h1>
-          <p className="lg:text-[2.8rem] md:text-[2.3rem] text-[1.8rem] font-black mt-4 italic text-clr-princ font-Genty">
-            "The future lies beyond the stars..."
-          </p>
+          {/* Motion container with stagger */}
+          <motion.div
+            className="flex flex-col items-center justify-center"
+            initial="hidden"
+            animate="show"
+            variants={staggerContainer} // Applying the stagger effect to the container
+          >
+            {/* Animated H1 */}
+            <motion.h1
+              className="lg:text-[5.5rem] md:text-[4.3rem] text-[3.7rem] font-black mt-8 text-white"
+              variants={fadeUp} // Applying fade-up animation to h1
+            >
+              Welcome to StarLight!
+            </motion.h1>
+
+            {/* Animated Paragraph */}
+            <motion.p
+              className="lg:text-[2.8rem] md:text-[2.3rem] text-[1.8rem] font-black mt-4 italic text-clr-princ font-Genty"
+              variants={fadeUp} // Applying fade-up animation to p
+            >
+              "The future lies beyond the stars..."
+            </motion.p>
+          </motion.div>
         </div>
       </div>
     </header>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,5 @@
 import LogoStarLight from "../assets/LogoStarLight.png";
-
 import React from "react";
-
 import {
   BookMarked,
   Eye,
@@ -9,10 +7,17 @@ import {
   MonitorDown,
   MonitorSmartphone,
 } from "lucide-react";
-
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion"; // Importing motion from Framer Motion
 
 const NavBar = () => {
+  // Animation settings
+  const fadeUp = {
+    initial: { opacity: 0, y: 50 },
+    animate: { opacity: 1, y: 0 },
+    transition: { type: "spring", stiffness: 100, damping: 25 },
+  };
+
   return (
     <div
       className="mr-auto ml-auto max-w-[1440px] relative z-20 bg-transparent py-6"
@@ -21,22 +26,25 @@ const NavBar = () => {
       <div className="relative z-20 bg-transparent">
         <div className="md:px-12 lg:py-2">
           <div className="flex items-center justify-between">
-            <div className="relative z-20 ml-6 lg:ml-0">
+            <motion.div className="relative z-20 ml-6 lg:ml-0" {...fadeUp}>
               <Link to="/">
                 <img src={LogoStarLight} alt="Logo1" className="lg:w-48 w-40" />
               </Link>
-            </div>
+            </motion.div>
 
             <div className="flex items-center justify-end mr-6 lg:ml-0">
               <div className="peer-checked:translate-x-0 fixed inset-0 w-[calc(100%-4.5rem)] translate-x-[-100%] bg-transparent shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0">
                 <div className="flex flex-col h-full justify-between lg:items-center lg:flex-row">
-                  <ul className="px-6 pt-32 text-white space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0">
+                  <motion.ul
+                    className="px-6 pt-32 text-white space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0"
+                    {...fadeUp}
+                  >
                     <li>
                       <Link
                         to="/Members"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 flex flex-row"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300">
                           Members
                         </span>
                       </Link>
@@ -46,7 +54,7 @@ const NavBar = () => {
                         to="/Events"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 flex flex-row"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300">
                           Events
                         </span>
                       </Link>
@@ -56,7 +64,7 @@ const NavBar = () => {
                         to="/Awards"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300">
                           Awards
                         </span>
                       </Link>
@@ -66,7 +74,7 @@ const NavBar = () => {
                         to="/Robots"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300">
                           Robots
                         </span>
                       </Link>
@@ -76,19 +84,23 @@ const NavBar = () => {
                         to="/About"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300">
                           About
                         </span>
                       </Link>
                     </li>
-                  </ul>
-                  <Link to="/Contact">
-                    <button className="font-Poppins text-white font-bold opacity-80 border-solid border-2 border-gray-500 hover:opacity-100 hover:border-white transition ease-in-out duration-300 rounded-xl text-center px-12 py-3">
-                      Contact
-                    </button>
-                  </Link>
+                  </motion.ul>
+
+                  <motion.div {...fadeUp}>
+                    <Link to="/Contact">
+                      <button className="font-Poppins text-white font-bold opacity-80 border-solid border-2 border-gray-500 hover:opacity-100 hover:border-white transition ease-in-out duration-300 rounded-xl text-center px-12 py-3">
+                        Contact
+                      </button>
+                    </Link>
+                  </motion.div>
                 </div>
               </div>
+
               <input
                 type="checkbox"
                 name="hamburger"
@@ -97,7 +109,7 @@ const NavBar = () => {
                 hidden
               />
               <label
-                for="hamburger"
+                htmlFor="hamburger"
                 className="peer-checked:hamburger block relative z-20 p-6 -mr-6 cursor-pointer lg:hidden"
               >
                 <div
@@ -112,13 +124,16 @@ const NavBar = () => {
 
               <div className="peer-checked:translate-x-0 fixed inset-0 w-[calc(100%-4.5rem)] translate-x-[-100%] bg-[rgb(14,14,15)] shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0 lg:hidden">
                 <div className="flex flex-col h-full justify-between lg:items-center lg:flex-row mt-16 md:mt-16 lg:mt-0">
-                  <ul className="px-6 pt-32 text-white space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0">
+                  <motion.ul
+                    className="px-6 pt-32 text-white space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0"
+                    {...fadeUp}
+                  >
                     <li>
                       <Link
                         to="/Members"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 flex flex-row"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
                           <Heart />
                           Members
                         </span>
@@ -129,7 +144,7 @@ const NavBar = () => {
                         to="/Events"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 flex flex-row"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
                           <Eye />
                           Events
                         </span>
@@ -140,7 +155,7 @@ const NavBar = () => {
                         to="/Awards"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
                           <MonitorDown />
                           Awards
                         </span>
@@ -151,7 +166,7 @@ const NavBar = () => {
                         to="/Robots"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
                           <BookMarked />
                           Robots
                         </span>
@@ -162,19 +177,21 @@ const NavBar = () => {
                         to="/About"
                         className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0"
                       >
-                        <span class="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
+                        <span className="relative text-white opacity-80 hover:opacity-100 transition duration-300 flex flex-row gap-2">
                           <MonitorSmartphone />
                           About
                         </span>
                       </Link>
                     </li>
                     <div className="w-full h-[1px] bg-gray-500"></div>
-                    <Link to="/Contact">
-                      <button className="mt-8 font-Poppins font-bold opacity-80 border-solid border-2 border-gray-500 hover:opacity-100 hover:border-white transition ease-in-out duration-300 rounded-xl text-center px-12 py-3">
-                        Contact
-                      </button>
-                    </Link>
-                  </ul>
+                    <motion.div {...fadeUp}>
+                      <Link to="/Contact">
+                        <button className="mt-8 font-Poppins font-bold opacity-80 border-solid border-2 border-gray-500 hover:opacity-100 hover:border-white transition ease-in-out duration-300 rounded-xl text-center px-12 py-3">
+                          Contact
+                        </button>
+                      </Link>
+                    </motion.div>
+                  </motion.ul>
                 </div>
               </div>
             </div>
